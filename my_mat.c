5ddd902c7423cf6258b557048c288d6ccc38b144
Dvir Borochov <dvir.borochov@gmail.com>
2021-11-20 17:49:49 +0200

    tey

--- my_mat.c
+++ my_mat.c
@@ -5,19 +5,15 @@ int mat[SIZE][SIZE];
 
 int min(int a, int b)
 {
-    if (a == 0)
-    {
-        return b;
-    }
-    if (b == 0)
-    {
-        return a;
-    }
     if (a == 0 && b == 0)
     {
         return 0;
     }
-    if (a < b)
+    if (a == 0)
+    {
+        return b;
+    }
+    if (b == 0 || a < b)
     {
         return a;
     }
